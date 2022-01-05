8a04a9d8281a473405628053e1b48e4cf301702a
swapnilsparsh <arezona.lucky55@gmail.com>
2022-01-06 02:01:31 +0530

    Tenor to GIPHY

--- commands/gif.js
+++ commands/gif.js
@@ -9,9 +9,10 @@ module.exports = async function (msg, args) {
 
     searchKeyword = args.join(" ");
   }
-  let url = `https://g.tenor.com/v1/search?q=${searchKeyword}&key=${process.env.TENORKEY}&contentfilter=high`;
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.TENORKEY}&q=${searchKeyword}&limit=5&offset=0&rating=g&lang=en`;
+  
   let response = await fetch(url);
   let json = await response.json();
-  const index = Math.floor(Math.random() * json.results.length);
-  msg.channel.send(json.results[index].url);
+  const index = Math.floor(Math.random() * json.data.length);
+  msg.channel.send(json.data[index].url);
 };
